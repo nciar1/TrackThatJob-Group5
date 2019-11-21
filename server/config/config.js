3200ea29a1b04f61998e32fe820ad66349702392
Nicole <nciar1@ufl.edu>
2019-11-21 16:23:36 -0500

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     db: {
-        uri: '', //place the URI of your mongo database here.
+        uri: 'mongodb+srv://dbUser:dbPassword@cluster0-cwgg0.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
     }
 };
