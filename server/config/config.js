595a51f5364f68063d6859a6f2ddcf57b8dba701
Nicole <nciar1@ufl.edu>
2019-11-24 12:57:51 -0500

    Revert "Update config.js"
    This reverts commit 3200ea29a1b04f61998e32fe820ad66349702392.

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     db: {
-        uri: 'mongodb+srv://dbUser:dbPassword@cluster0-cwgg0.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+        uri: '', //place the URI of your mongo database here.
     }
 };
