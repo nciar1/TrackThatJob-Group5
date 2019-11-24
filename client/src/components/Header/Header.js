95026be39efd2e2eefc3f33fbeb8eaa15b9f8f4f
Sara Marigomen <saramarigomen@gmail.com>
2019-11-24 17:48:14 -0500

    small navbar edit

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -14,20 +14,15 @@ const Header = () => {
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
             <script src="https://code.angularjs.org/1.4.3/angular.js"></script>
             
-            <link rel="stylesheet" href="index.css"/>
-            
             <nav class="navbar fixed-top navbar-default">
                 <div class="container-fluid">
+                    
                     <div class="navbar-header">
-                        <a class="navbar-brand" href="/">Logo</a>
+                        <a class="navbar-brand" href="/">Home</a>
                     </div>
                     
-                   <ul class="nav navbar-nav">
-                        <li><a href="/">Home</a></li>
-                    </ul>
-                    
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Dashboard</a></li>
                         <li><a href="./AppTracker">Track That Job</a></li>
                         <li><a href="./MyProfile">My Profile</a></li>
                     </ul>
