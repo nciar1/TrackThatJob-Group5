c6be373604ded9d72385578ebfc743cc0de02055
Gerrad Hardy <gerakey@ufl.edu>
2019-11-24 17:01:41 -0500

    Fixed npm install issue

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
-import {MyProfile} from './components/MyProfile';
 import AppTracker from "./views/AppTracker/AppTrackerView"
 import Header from "./components/Header/Header"
 import ProfilePage from "./views/ProfilePage/ProfilePage"
